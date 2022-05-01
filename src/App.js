dfa19c0983c6f7a922ab873c83665790d4099ad6
Nick Paduchowski <82173102+nick-paduchowski@users.noreply.github.com>
2022-05-01 03:17:01 -0400

    Started development on posts page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import Nav from "./components/Navigation/Nav";
 import Home from "./components/Home/Home";
+import Posts from "./components/Posts//Posts"
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
         <Nav />
         <Routes>
           <Route path="/" exact element={<Home />} />
+          <Route path="/posts" exact element={<Posts />} />
         </Routes>
       </div>
     </BrowserRouter>
